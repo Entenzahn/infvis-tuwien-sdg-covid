85cf320ef1b0ecee639bbc2f19293ccb6e313935
Alice M <alice.martzloff@gmail.com>
2021-06-04 15:25:46 +0200

    Added india.json file Changed the titel (again for test) Midification of map file to display the map

--- static/js/map.js
+++ static/js/map.js
@@ -4,15 +4,15 @@ let map = null;
 let mapData = null;
 
 
-function initMap(data) {
+function initMap() {
 
     // loads the world map as topojson
-    d3.json("../static/data/world-topo.json").then(function (countries) {
+    d3.json("../scripts/data/india.json").then(function (countries) {
 
         // defines the map projection method and scales the map within the SVG
         let projection = d3.geoEqualEarth()
-            .scale(180)
-            .translate([mapWidth / 2, mapHeight / 2]);
+            .scale(1300)
+            .translate([-1.5*mapWidth, 1.15*mapHeight]);
 
         // generates the path coordinates from topojson
         let path = d3.geoPath()
@@ -24,7 +24,7 @@ function initMap(data) {
             .attr("height", mapHeight);
 
         // map geometry
-        mapData = topojson.feature(countries, countries.objects.countries).features;
+        mapData = topojson.feature(countries, countries.objects.IND_adm1).features;
 
 
         // generates and styles the SVG path
@@ -36,17 +36,15 @@ function initMap(data) {
             .attr("stroke", "black")
             .attr("stroke-width", 0.5)
             .attr("fill-opacity",0.8)
-            .attr("fill", function(d){ if (d3.values(data.LOCATION).includes(d.properties.id)){
-                                          return "grey"
-                                     } else return "white"}) //OECD data gets special coloring
+            .attr("fill", "white")
             //For each country that is present in our OECD data:
-            .each(function (d) {if (d3.values(data.LOCATION).includes(d.properties.id)){
+            /*.each(function (d) {if (d3.values(data.LOCATION).includes(d.properties.id)){
             d3.select(this)
             .attr("country", function(d){ return d.properties.id}) //Store country name as attribute for easy reference
             .classed("oecd", true) //Add OECD class
             .on("mouseover", highlightCountry) //Add event behaviours
             .on("mousemove", move_tooltip)
-            .on("mouseout", country_mouseout)}});
+            .on("mouseout", country_mouseout)}});*/
 
     });
 
