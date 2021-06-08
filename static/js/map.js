bb539c12f152c137ee9aeb8d41ba96ffb4454be6
Alice M <alice.martzloff@gmail.com>
2021-06-08 14:09:12 +0200

    test

--- static/js/map.js
+++ static/js/map.js
@@ -1,16 +1,20 @@
 let mapWidth = 1105;
 let mapHeight = 800;
+let map = null;
 
 function initMap() {
     // Loads the indian svg
         // Accesses the image document in xml language
     d3.xml("static/data/india.svg").then(function (data) {
+
         // Selects the right div for the map
-        d3.select("#svg_map")
+        let svg = d3.select("#svg_map")
         // Scales the svg container
-        .attr("width", mapWidth)
-        .attr("height", mapHeight)
+            .attr("width", mapWidth)
+            .attr("height", mapHeight)
         // Attaches the xml data to the div
-        .node().append(data.documentElement)
+            .node().append(data.documentElement)
+
+        //let map = d3.select("#svg2").attr("style", "color: red");
         });
 }
